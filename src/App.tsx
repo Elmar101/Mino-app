f4cab0c26f8c8ab4f832f4febb7288bb3bd6f44b
elmar.amanov <elmar.amanov@cybernet.az>
2023-10-08 01:26:04 +0400

    How to use Link rect router element in my app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Post from './pages/Post';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
  
@@ -10,6 +10,11 @@ function App() {
 
   return (
     <div className="App">
+      <ul>
+        <li><Link to="/" >Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/post">Post</Link></li>
+      </ul>
       <Routes>
         <Route path='/' element={<Home/> } />
         <Route path='/about' element={ <About/> } />
