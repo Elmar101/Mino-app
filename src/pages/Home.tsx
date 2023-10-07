71d8983fcb33dd5cf1cce7f39f7b79d227fe3699
elmar.amanov <elmar.amanov@cybernet.az>
2023-10-08 01:53:30 +0400

    input onchange event add same value

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,39 @@
 import { useState } from "react";
 
+interface IUser {
+  name: string;
+  surname: string;
+};
+
 const Home = () => {
-  const [state, setState] = useState<{title: string}>({title: "Home"});
+  const [state, setState] = useState<IUser[]>([]);
+  const [name, setName] = useState<string>("");
+  const [suname, setSurname] = useState<string>("");
+
+
+  const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(name, suname);
+    
+  };
+
 
   return (
-    <div>{state.title}</div>
+    <>
+      <form onSubmit={onHandleSubmit}>
+        Name: <input value={name} onChange={()=> setName("Elmar")}/> <br/>
+        Surname: <input value={suname} onChange={()=> setSurname("Amanov") }/> <br/>
+        <button type="submit"> Add user </button>
+      </form>
+      <br/><br/>
+      <div>
+        <h1> Show All Users</h1>
+        {state && state.map(obj=> <div>
+          <h6>{obj.name}</h6>
+          <h6>{obj.surname}</h6>
+        </div>)}
+      </div>
+    </>
   );
 
 }
