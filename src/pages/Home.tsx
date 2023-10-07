37431aa360f1623de139cb3ed666fc9f6413e229
elmar.amanov <elmar.amanov@cybernet.az>
2023-10-08 02:24:12 +0400

    update state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,38 +5,41 @@ interface IUser {
   surname: string;
 };
 
+const data: IUser[] = [
+  {name: '' , surname: ''}
+];
+
 const Home = () => {
-  const [state, setState] = useState<IUser[]>([]);
+  const [state, setState] = useState<IUser[]>(data);
   const [name, setName] = useState<string>("");
-  const [suname, setSurname] = useState<string>("");
+  const [surname, setSurname] = useState<string>("");
 
 
   const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(name, suname);
+    if(!name && !surname) return;
+    setState([...state, { name: name, surname: surname}]);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
     if(event.target.name === 'name') setName(event.target.value);
     else if(event.target.name === 'surname') setSurname(event.target.value);
   };
-    
-    
-
+  
+  console.log(!name && !surname);
 
   return (
     <>
       <form onSubmit={onHandleSubmit}>
         Name: <input name="name" value={name} onChange={handleChange}/> <br/>
-        Surname: <input name="surname" value={suname} onChange={handleChange}/> <br/>
-        <button type="submit"> Add user </button>
+        Surname: <input name="surname" value={surname} onChange={handleChange}/> <br/>
+        <button type="submit" disabled={!name || !surname}> Add user </button>
       </form>
       <br/><br/>
       <div>
         <h1> Show All Users</h1>
-        {state && state.map(obj=> <div>
-          <h6>{obj.name}</h6>
-          <h6>{obj.surname}</h6>
+        {state && state.map(obj=> <div key={obj.name}>
+          <h6>{obj.name} {obj.surname}</h6>
         </div>)}
       </div>
     </>
